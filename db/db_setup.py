f820095af87afd26dd0f6eb92277f71a8af21608
hazyd <wl9dk@virginia.edu>
2020-03-10 20:17:15 -0700

    Add space

--- db/db_setup.py
+++ db/db_setup.py
@@ -50,3 +50,4 @@ Base.metadata.bind = engine
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
+
